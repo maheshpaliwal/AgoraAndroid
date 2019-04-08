51f62a5a9cc2919da99e97955f88ca76ea7a91bb
MAHESH PALIWAL <39883849+maheshpaliwal@users.noreply.github.com>
2019-04-09 05:18:52 +0530

    Link to sign In Activity

--- app/src/main/java/com/maheshpaliwal/agora_android/PasswordResetEmailSent.kt
+++ app/src/main/java/com/maheshpaliwal/agora_android/PasswordResetEmailSent.kt
@@ -1,12 +1,24 @@
 package com.maheshpaliwal.agora_android
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PasswordResetEmailSent : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_password_reset_email_sent)
+
+
+        val redirect_signIn:Button=findViewById<Button>(R.id.btn_signin)
+        redirect_signIn.setOnClickListener{
+
+            startActivity(Intent(this@PasswordResetEmailSent,SignInActivity::class.java))
+
+        }
     }
 }
